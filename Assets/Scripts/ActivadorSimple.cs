138b0dfc80dd969155bcb4ebe986dd013ab58c20
Pablo03-dev <pablo.villegasa@alumnos.arcos.cl>
2023-05-14 16:18:08 -0400

    seniugr

---
+++ Assets/Scripts/ActivadorSimple.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ActivadorSimple : MonoBehaviour
+{
+    [SerializeField] private UnityEvent Cortado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Melee"))
+        {
+            Cortado?.Invoke();
+        }
+    }
+}
